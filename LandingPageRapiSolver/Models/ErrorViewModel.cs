02760fb5f86ec5139c8125c9fc58b93873cd1401
Piero <u20161c138@upc.edu.pe>
2019-10-22 00:56:21 -0500

    Creacion de LandingPage

---
+++ LandingPageRapiSolver/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LandingPageRapiSolver.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
